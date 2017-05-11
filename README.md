bb93c97bc75e8feee71229ce527de21af973f12f
varshareddydvr <varshareddy.dvr@gmail.com>
2017-05-10 22:16:02 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Getting_and_Cleaning_Data_CourseProject
+Getting and Cleaning Data Course Project:
+
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis.
+
+The run_analysis.R script performs the following operations:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+Steps required to run the script:
+
+1. Download the data source, unzipping it and moving it to your local drive. UCI HAR Dataset is the unzipped folder.
+2. Place run_analysis.R in the parent folder of UCI HAR Dataset, then set it as your working directory using setwd() function in RStudio.
+3. run_analysis.R file will install/load the required packages(reshape2 and data.table) automatically.
+4. When we run source("run_analysis.R"), then it will generate a new file tidy_data.txt in your working directory.
---
+++ run_analysis.R
@@ -0,0 +1,67 @@
+
+# installing or loading the required packages to run the script
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+require("data.table")
+require("reshape2")
+
+##zip file is downloaded, saved under working directory and unzipped
+# Loading activity labels
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")[,2]
+
+# Loading column names of data 
+features <- read.table("UCI HAR Dataset/features.txt")[,2]
+
+# Extracting only the measurements on the mean and standard deviation for each measurement
+extract_features <- grepl("mean\\(\\)|std\\(\\)", features)
+
+# Load and process X_train & y_train data.
+Xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+Ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+SubjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+names(Xtrain) <- features
+
+# Load and process test data.
+Xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
+Ytest <- read.table("UCI HAR Dataset/test/y_test.txt")
+SubjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+names(Xtest) <- features
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+Xtrain <- Xtrain[,extract_features]
+Xtest <- Xtest[,extract_features]
+
+# Using descriptive activity names to name the activities in the data set
+Ytrain[,2] <- activity_labels[Ytrain[,1]]
+Ytest[,2] <- activity_labels[Ytest[,1]]
+
+# Appropriately labeling the data set with descriptive variable names.
+names(Ytrain) <- c("ActivityID", "ActivityLabel")
+names(SubjectTrain) <- "Subject"
+names(Ytest) <- c("ActivityID", "ActivityLabel")
+names(SubjectTest) <- "Subject"
+
+# Bind data
+train_data <- cbind(as.data.table(SubjectTrain), Ytrain, Xtrain)
+test_data <- cbind(as.data.table(SubjectTest), Ytest, Xtest)
+
+# Merging the training and the test sets to create one data set.
+data <- rbind(train_data, test_data)
+
+# Melt the data into a form suitable for easy casting
+labels   = c("Subject", "ActivityID", "ActivityLabel")
+data_labels = setdiff(colnames(data), labels)
+melt_data      = melt(data, id = labels, measure.vars = data_labels)
+
+# From the tidy data set, creating a data set with the average of each variable for each activity and each subject
+tidy_data   <- dcast(melt_data, Subject + ActivityLabel ~ variable, mean)
+
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
